e1bd8bb86e395583edb933fba919fdcd483bf2cc
CaseyCrumb <caseyncrumb@gmail.com>
2023-10-18 18:30:07 -0500

    commit changes

--- src/components/BarChart.jsx
+++ src/components/BarChart.jsx
@@ -39,8 +39,8 @@ const BarChart = ({ isDashboard = false }) => {
           },
         },
       }}
-      keys={["hot dog", "burger", "sandwich", "kebab", "fries", "donut"]}
-      indexBy="country"
+      keys={["Stage 1", "Stage 2", "Stage 3", "Stage 4", "Stage 5"]}
+      indexBy="Stage"
       margin={{ top: 50, right: 130, bottom: 50, left: 60 }}
       padding={0.3}
       valueScale={{ type: "linear" }}
@@ -76,7 +76,7 @@ const BarChart = ({ isDashboard = false }) => {
         tickSize: 5,
         tickPadding: 5,
         tickRotation: 0,
-        legend: isDashboard ? undefined : "country", // changed
+        legend: isDashboard ? undefined : "Stage", // changed
         legendPosition: "middle",
         legendOffset: 32,
       }}
@@ -84,7 +84,7 @@ const BarChart = ({ isDashboard = false }) => {
         tickSize: 5,
         tickPadding: 5,
         tickRotation: 0,
-        legend: isDashboard ? undefined : "food", // changed
+        legend: isDashboard ? undefined : "Projects", // changed
         legendPosition: "middle",
         legendOffset: -40,
       }}
